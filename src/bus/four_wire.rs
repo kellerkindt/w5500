b9ba375ca2391b00cbfb96d4b4336a0c977fe15d
Ryan Summers <ryan.summers@vertigo-designs.com>
2024-07-01 17:54:13 +0200

    Refactoring bus traits for embedded-hal 1.0

--- src/bus/four_wire.rs
+++ src/bus/four_wire.rs
@@ -1,8 +1,7 @@
 #![allow(clippy::inconsistent_digit_grouping, clippy::unusual_byte_groupings)]
 
 use core::fmt;
-use embedded_hal::blocking::spi::{Transfer, Write};
-use embedded_hal::digital::v2::OutputPin;
+use embedded_hal::spi::{ErrorType, Operation, SpiDevice};
 
 use crate::bus::Bus;
 
@@ -11,99 +10,49 @@ const WRITE_MODE_MASK: u8 = 0b00000_1_00;
 // TODO This name is not ideal, should be renamed to VDM
 #[derive(Debug)]
 #[cfg_attr(feature = "defmt", derive(defmt::Format))]
-pub struct FourWire<Spi: Transfer<u8> + Write<u8>, ChipSelect: OutputPin> {
-    cs: ChipSelect,
-    spi: Spi,
+pub struct FourWire<SPI> {
+    spi: SPI,
 }
 
-impl<Spi: Transfer<u8> + Write<u8>, ChipSelect: OutputPin> FourWire<Spi, ChipSelect> {
-    pub fn new(spi: Spi, cs: ChipSelect) -> Self {
-        Self { cs, spi }
+impl<SPI> FourWire<SPI> {
+    pub fn new(spi: SPI) -> Self {
+        Self { spi }
     }
 
-    pub fn release(self) -> (Spi, ChipSelect) {
-        (self.spi, self.cs)
+    pub fn release(self) -> SPI {
+        self.spi
     }
 }
 
-impl<Spi: Transfer<u8> + Write<u8>, ChipSelect: OutputPin> Bus for FourWire<Spi, ChipSelect> {
-    type Error =
-        FourWireError<<Spi as Transfer<u8>>::Error, <Spi as Write<u8>>::Error, ChipSelect::Error>;
+impl<SPI: SpiDevice> Bus for FourWire<SPI> {
+    type Error = <SPI as ErrorType>::Error;
 
-    fn read_frame(&mut self, block: u8, address: u16, data: &mut [u8]) -> Result<(), Self::Error> {
+    fn read_frame(&mut self, block: u8, address: u16, data: &mut [u8]) -> Result<(), SPI::Error> {
         let address_phase = address.to_be_bytes();
         let control_phase = block << 3;
-        let data_phase = data;
 
-        // set Chip select to Low, i.e. prepare to receive data
-        self.cs.set_low().map_err(FourWireError::ChipSelectError)?;
-        let result = (|| {
-            self.spi
-                .write(&address_phase)
-                .and_then(|_| self.spi.write(&[control_phase]))
-                .map_err(FourWireError::WriteError)?;
-            self.spi
-                .transfer(data_phase)
-                .map_err(FourWireError::TransferError)?;
-            Ok(())
-        })();
+        self.spi.transaction(&mut [
+            Operation::Write(&address_phase),
+            Operation::Write(&[control_phase]),
+            Operation::Write(data),
+        ])?;
 
-        // set Chip select to High, i.e. we've finished listening
-        self.cs.set_high().map_err(FourWireError::ChipSelectError)?;
-
-        // then return the result of the transmission
-        result
+        Ok(())
     }
 
-    fn write_frame(&mut self, block: u8, address: u16, data: &[u8]) -> Result<(), Self::Error> {
-        let address_phase = address.to_be_bytes();
+    fn write_frame(&mut self, block: u8, address: u16, data: &[u8]) -> Result<(), SPI::Error> {
         let control_phase = block << 3 | WRITE_MODE_MASK;
-        let data_phase = data;
 
-        // set Chip select to Low, i.e. prepare to transmit
-        self.cs.set_low().map_err(FourWireError::ChipSelectError)?;
-        let result = self
-            .spi
-            .write(&address_phase)
-            .and_then(|_| self.spi.write(&[control_phase]))
-            .and_then(|_| self.spi.write(data_phase))
-            .map_err(FourWireError::WriteError);
+        let address_phase = address.to_be_bytes();
+        self.spi.transaction(&mut [
+            Operation::Write(&address_phase),
+            Operation::Write(&[control_phase]),
+            Operation::Write(data),
+        ])?;
 
-        // set Chip select to High, i.e. we've finished transmitting
-        self.cs.set_high().map_err(FourWireError::ChipSelectError)?;
-
-        // then return the result of the transmission
-        result
+        Ok(())
     }
 }
-
-// Must use map_err, ambiguity prevents From from being implemented
-#[repr(u8)]
-#[derive(Clone)]
-pub enum FourWireError<TransferError, WriteError, ChipSelectError> {
-    TransferError(TransferError),
-    WriteError(WriteError),
-    ChipSelectError(ChipSelectError),
-}
-
-impl<TransferError, WriteError, ChipSelectError> fmt::Debug
-    for FourWireError<TransferError, WriteError, ChipSelectError>
-{
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(
-            f,
-            "FourWireError::{}",
-            match self {
-                Self::TransferError(_) => "TransferError",
-                Self::WriteError(_) => "WriteError",
-                Self::ChipSelectError(_) => "ChipSelectError",
-            }
-        )
-    }
-}
-
-// TODO Improved error rendering could be done with specialization.
-// https://github.com/rust-lang/rust/issues/31844
 
 #[cfg(test)]
 mod test {
--- src/bus/mod.rs
+++ src/bus/mod.rs
@@ -1,14 +1,9 @@
 use core::fmt::Debug;
 
 mod four_wire;
-mod four_wire_ref;
 mod three_wire;
 
 pub use self::four_wire::FourWire;
-pub use self::four_wire::FourWireError;
-pub use self::four_wire_ref::FourWireRef;
-pub use self::four_wire_ref::OutputPinRef;
-pub use self::four_wire_ref::SpiRef;
 pub use self::three_wire::ThreeWire;
 pub use self::three_wire::ThreeWireError;
 
@@ -18,20 +13,4 @@ pub trait Bus {
     fn read_frame(&mut self, block: u8, address: u16, data: &mut [u8]) -> Result<(), Self::Error>;
 
     fn write_frame(&mut self, block: u8, address: u16, data: &[u8]) -> Result<(), Self::Error>;
-}
-
-pub struct BusRef<'a, B: Bus>(pub &'a mut B);
-
-impl<B: Bus> Bus for BusRef<'_, B> {
-    type Error = B::Error;
-
-    #[inline]
-    fn read_frame(&mut self, block: u8, address: u16, data: &mut [u8]) -> Result<(), Self::Error> {
-        self.0.read_frame(block, address, data)
-    }
-
-    #[inline]
-    fn write_frame(&mut self, block: u8, address: u16, data: &[u8]) -> Result<(), Self::Error> {
-        self.0.write_frame(block, address, data)
-    }
 }
