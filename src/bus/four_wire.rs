00876529ed8e214a3cd167be3299126aec841d6e
Ryan Summers <ryan.summers@vertigo-designs.com>
2024-07-01 18:07:48 +0200

    Fixing docs and tests

--- src/bus/four_wire.rs
+++ src/bus/four_wire.rs
@@ -34,7 +34,7 @@ impl<SPI: SpiDevice> Bus for FourWire<SPI> {
         self.spi.transaction(&mut [
             Operation::Write(&address_phase),
             Operation::Write(&[control_phase]),
-            Operation::Write(data),
+            Operation::TransferInPlace(data),
         ])?;
 
         Ok(())
@@ -56,11 +56,7 @@ impl<SPI: SpiDevice> Bus for FourWire<SPI> {
 
 #[cfg(test)]
 mod test {
-    use embedded_hal::digital::v2::OutputPin;
-    use embedded_hal_mock::{
-        pin::{Mock as PinMock, State as PinState, Transaction as PinTransaction},
-        spi::{Mock as SpiMock, Transaction as SpiTransaction},
-    };
+    use embedded_hal_mock::eh1::spi::{Mock as SpiMock, Transaction as SpiTransaction};
 
     use crate::{
         bus::{four_wire::WRITE_MODE_MASK, Bus},
@@ -71,30 +67,20 @@ mod test {
 
     #[test]
     fn test_read_frame() {
-        let mut cs_pin = PinMock::new(&[
-            // we begin with pin HIGH
-            PinTransaction::set(PinState::High),
-            // When reading
-            PinTransaction::set(PinState::Low),
-            // When finished reading
-            PinTransaction::set(PinState::High),
-        ]);
-
-        // initiate the pin to high.
-        cs_pin.set_high().expect("Should set pin to high");
-
         let mut actual_version = [0_u8; 1];
         let mut expected_version = 5;
 
         let expectations = [
-            SpiTransaction::write(register::common::VERSION.to_be_bytes().to_vec()),
-            SpiTransaction::write(vec![register::COMMON << 3]),
-            SpiTransaction::transfer(actual_version.to_vec(), vec![expected_version]),
+            SpiTransaction::transaction_start(),
+            SpiTransaction::write_vec(register::common::VERSION.to_be_bytes().to_vec()),
+            SpiTransaction::write(register::COMMON << 3),
+            SpiTransaction::transfer_in_place(actual_version.to_vec(), vec![expected_version]),
+            SpiTransaction::transaction_end(),
         ];
 
         let mock_spi = SpiMock::new(&expectations);
 
-        let mut four_wire = FourWire::new(mock_spi, cs_pin);
+        let mut four_wire = FourWire::new(mock_spi);
 
         four_wire.read_frame(
             register::COMMON,
@@ -102,41 +88,35 @@ mod test {
             &mut actual_version,
         );
 
+        four_wire.release().done();
+
         assert_eq!(expected_version, actual_version[0]);
     }
 
     #[test]
     fn test_write_frame() {
-        let mut cs_pin = PinMock::new(&[
-            // we begin with pin HIGH
-            PinTransaction::set(PinState::High),
-            // When reading
-            PinTransaction::set(PinState::Low),
-            // When finished reading
-            PinTransaction::set(PinState::High),
-        ]);
-
-        // initiate the pin to high.
-        cs_pin.set_high().expect("Should set pin to high");
-
         let socket_0_reg = 0x01_u8;
         let socket_1_reg = 0x05_u8;
         let source_port = 49849_u16;
 
         let expectations = [
-            SpiTransaction::write(register::socketn::SOURCE_PORT.to_be_bytes().to_vec()),
-            SpiTransaction::write(vec![socket_1_reg << 3 | WRITE_MODE_MASK]),
-            SpiTransaction::write(source_port.to_be_bytes().to_vec()),
+            SpiTransaction::transaction_start(),
+            SpiTransaction::write_vec(register::socketn::SOURCE_PORT.to_be_bytes().to_vec()),
+            SpiTransaction::write(socket_1_reg << 3 | WRITE_MODE_MASK),
+            SpiTransaction::write_vec(source_port.to_be_bytes().to_vec()),
+            SpiTransaction::transaction_end(),
         ];
 
         let mock_spi = SpiMock::new(&expectations);
 
-        let mut four_wire = FourWire::new(mock_spi, cs_pin);
+        let mut four_wire = FourWire::new(mock_spi);
 
         four_wire.write_frame(
             socket_1_reg,
             register::socketn::SOURCE_PORT,
             &source_port.to_be_bytes(),
         );
+
+        four_wire.release().done();
     }
 }
